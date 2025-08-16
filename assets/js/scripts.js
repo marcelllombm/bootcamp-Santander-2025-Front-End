3448612af1e1202b5e8a42d92847d15d4e10909d
Marcelllombm <marceloborgesdemelo@macminimarcelllombm.local>
2025-08-16 10:12:49 -0300

    feat: add footer

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -26,3 +26,12 @@ accordionHeades.forEach((header) => {
       : accordionItem.classList.add("active");
   });
 });
+
+const menuLinks = document.querySelectorAll(".menu__link");
+
+menuLinks.forEach((item) => {
+  item.addEventListener("click", () => {
+    menuLinks.forEach((i) => i.classList.remove("active"));
+    item.classList.add("active");
+  });
+});
